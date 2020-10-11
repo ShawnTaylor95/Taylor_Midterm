f9abe302973eaa2c11f2dbc51a53780c4ee8d6d8
ShawnTaylor95 <Taylor_Shawn2@Columbusstate.edu>
2020-10-11 07:00:42 +0000

    basic code

---
+++ index.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var fetch = require('node-fetch');
+
+var app = express();
+
+//Port information
+const port = process.env.port || 3000;
+
+//tell application to use ejs for templates
+app.set('view engine', 'ejs');
+//make styles public
+app.use(express.static("public"));
+
+app.get('/', function(req,res){
+//return something to homepage
+    res.send('Hello World!');
+
+});
+
+
+//Server setup
+app.listen(port,function(){
+    console.log('Listening on ' + port)
+});
