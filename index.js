0adc9a45c9caa61544499166948a0d37424fcda8
ShawnTaylor95 <Taylor_Shawn2@Columbusstate.edu>
2020-10-11 16:02:48 +0000

    added api for current comic

--- index.js
+++ index.js
@@ -17,6 +17,16 @@ app.get('/', function(req,res){
 
 });
 
+app.get('/comic', function(req,res){
+    let todayData;
+    fetch('http://xkcd.com/info.0.json')
+    .then(res => res.json())
+    .then(data => {
+        todayData = data;
+        res.json(todayData);
+    });
+});
+
 
 //Server setup
 app.listen(port,function(){
